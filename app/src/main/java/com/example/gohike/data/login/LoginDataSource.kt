593939b8516ceaabd4adcf37425dead01cad081b
Evghenii Zaharov <evghenii.zaharov01@gmail.com>
2023-04-01 18:49:15 +0300

    Added Log in

---
+++ app/src/main/java/com/example/gohike/data/login/LoginDataSource.kt
@@ -0,0 +1,65 @@
+package com.example.gohike.data.login
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.example.gohike.connection.ConnectionThread
+import com.example.gohike.connection.login.LoginFailureEvent
+import com.example.gohike.connection.login.LoginHandle
+import com.example.gohike.connection.login.LoginRequestEvent
+import com.example.gohike.connection.login.LoginSuccessEvent
+import com.example.gohike.connection.register.RegistrationFailureEvent
+import com.example.gohike.connection.register.RegistrationRequestEvent
+import com.example.gohike.connection.register.RegistrationSuccessEvent
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+class LoginDataSource {
+    // Firebase Authentification
+    private var auth: FirebaseAuth = Firebase.auth
+
+    init {
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+
+    fun login(event: LoginRequestEvent) {
+        auth.signInWithEmailAndPassword(event.email, event.password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    var loginSuccessEvent : LoginSuccessEvent = LoginSuccessEvent(auth.currentUser!!)
+                    ConnectionThread.addEvent(loginSuccessEvent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    var loginFailureEvent : LoginFailureEvent = LoginFailureEvent(auth.currentUser!!)
+                    ConnectionThread.addEvent(loginFailureEvent)
+                }
+            }
+    }
+
+    fun register(event: RegistrationRequestEvent) {
+        auth.createUserWithEmailAndPassword(event.email, event.password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    ConnectionThread.addEvent(RegistrationSuccessEvent(user = auth.currentUser!!))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    ConnectionThread.addEvent(RegistrationFailureEvent())
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/gohike/data/login/LoginRepository.kt
@@ -0,0 +1,94 @@
+package com.example.gohike.data.login
+
+import android.util.Log
+import com.example.gohike.connection.ConnectionThread
+import com.example.gohike.connection.login.LoginFailureEvent
+import com.example.gohike.connection.login.LoginHandle
+import com.example.gohike.connection.login.LoginRequestEvent
+import com.example.gohike.connection.login.LoginSuccessEvent
+import com.example.gohike.connection.register.RegistrationFailureEvent
+import com.example.gohike.connection.register.RegistrationHandle
+import com.example.gohike.connection.register.RegistrationRequestEvent
+import com.example.gohike.connection.register.RegistrationSuccessEvent
+import com.example.gohike.data.login.model.LoggedInUser
+import com.google.firebase.auth.FirebaseUser
+
+
+/**
+ * Class that requests authentication and user information from the remote data source and
+ * maintains an in-memory cache of login status and user credentials information.
+ */
+
+class LoginRepository(val dataSource: LoginDataSource) : LoginHandle, RegistrationHandle {
+    private val TAG = "LoginRepository"
+
+    // in-memory cache of the loggedInUser object
+    var user: FirebaseUser? = null
+        private set
+
+    val isLoggedIn: Boolean
+        get() = user != null
+
+    init {
+        // If user credentials will be cached in local storage, it is recommended it be encrypted
+        // @see https://developer.android.com/training/articles/keystore
+        user = null
+
+        Log.v(TAG, "In Login Repository init 1...")
+
+        ConnectionThread.addLoginHandler(this)
+        ConnectionThread.addRegistrationHandler(this)
+
+        Log.v(TAG, "In Login Repository init 2...")
+    }
+
+    fun logout() {
+        user = null
+        dataSource.logout()
+    }
+
+    override fun onLoginRequest(event: LoginRequestEvent) {
+        user?.let {
+            // User is already authenticated
+            Log.v(TAG, "User is already loged in")
+            ConnectionThread.addEvent(LoginSuccessEvent(it))
+            return@onLoginRequest
+        }
+
+        Log.v(TAG, "Log in attempt...")
+        // If user is not authenticated then request login
+        // from the data source
+        dataSource.login(event)
+    }
+
+    override fun onLoginSuccess(event: LoginSuccessEvent) {
+        user = event.user
+    }
+
+    override fun onLoginFailure(event: LoginFailureEvent) {
+        // Do nothing
+    }
+
+    override fun onRegistrationRequest(event: RegistrationRequestEvent) {
+        user?.let {
+            // User is already anthenticated
+            Log.v(TAG, "User cannot be registered because user is loged in")
+            ConnectionThread.addEvent(RegistrationSuccessEvent(it))
+            return@onRegistrationRequest
+        }
+
+        Log.v(TAG, "Registration attempt...")
+        // If user is not registered then request registration
+        // from the data source
+        dataSource.register(event)
+    }
+
+    override fun onRegistrationSuccess(event: RegistrationSuccessEvent) {
+        user = event.user
+    }
+
+    override fun onRegistrationFailure(event: RegistrationFailureEvent) {
+        // Do nothing
+    }
+
+}
